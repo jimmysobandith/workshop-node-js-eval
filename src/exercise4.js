11bc8fe8d587c596b23403fe6a5d5d6c2951dc03
Jimmy <jimmy.sobandith@outlook.fr>
2017-12-12 17:06:45 +0100

    Exercice 4

--- src/exercise4.js
+++ src/exercise4.js
@@ -1,8 +1,14 @@
 exports.run = function(input) {
-  console.log("Exercise #4");
-
-  // YOUR CODE IS WELCOME HERE
-
+	var myFunction = "The value of";
+	
+	function input(myFunction, unTableau, deuxiemeTableau)
+	{
+		var string = myFunction+" "+deuxiemeTableau[0]+" is "+unTableau[0]+", "+
+					myFunction+" "+deuxiemeTableau[1]+" is "+unTableau[1]+", "+
+					myFunction+" "+deuxiemeTableau[2]+" is "+unTableau[2]+"";
+	  return string;
+	}
+ 
   /** STRICTLY FORBIDDEN TO CHANGE THIS CODE */
   return input(myFunction, [1, 2, 3], ["One", "Two", "Three"]);
   /** */
